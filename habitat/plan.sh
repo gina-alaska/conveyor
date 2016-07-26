de6753411cc1f4d76f251420186637299e2e6b05
Scott Macfarlane <macfarlane.scott@gmail.com>
2016-07-26 09:30:58 -0800

    Update plans

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,14 +1,16 @@
 pkg_name="conveyor"
-pkg_version="1.0.1"
+pkg_version="1.0.2"
 pkg_origin="uafgina"
 pkg_maintainer="UAF GINA <support+habitat@gina.alaska.edu>"
 pkg_license=('MIT')
-pkg_source="https://github.com/gina-alaska/${pkg_name}/archive/v${pkg_version}.tar.gz"
-pkg_shasum="3b00ec91854a61c6ddfa0cb004ef06e17912dddd488bed0bf921611d4a7b47c1"
+pkg_source="https://github.com/gina-alaska/${pkg_name}/archive/${pkg_version}.tar.gz"
+pkg_shasum="f5a89c713699f5902d0f35939187cfd5c79dbdc728b31dd78a0914c8af5fb0ba"
 
 pkg_deps=(
   core/ruby
   core/bundler
+  core/gcc-libs
+  core/openssl
 )
 
 pkg_build_deps=(
@@ -17,23 +19,24 @@ pkg_build_deps=(
   core/git
   core/coreutils
   core/gcc
+  core/openssl
   core/make
 )
 
 pkg_bin_dirs=(bin)
 
 do_build() {
+  export CPPFLAGS="${CPPFLAGS} ${CFLAGS}"
   local _bundler_dir=$(pkg_path_for bundler)
 
   export GEM_HOME=${pkg_path}/vendor/bundler
   export GEM_PATH=${_bundler_dir}:${GEM_HOME}
 
-  bundle install --jobs 2 --retry 5 --path vendor/bundle --binstubs --without development test
+  bundle install --jobs 2 --retry 5 --path vendor/bundle --without development test
 }
 
 do_install() {
   cp -R . ${pkg_prefix}
-
 
   for binstub in ${pkg_prefix}/bin/*; do
     build_line "Setting shebang for ${binstub} to 'ruby'"
